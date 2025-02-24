73059fa22010c27c6ad61920d6b4cdfc700924e7
Syltyce <sylvain.gg.fr@gmail.com>
2025-02-24 21:03:41 +0100

    modif nom variable db

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,21 +2,23 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: process.env.DB_USER || process.env.MYSQLUSER,
+    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+    database: process.env.DB_NAME || process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST || process.env.MYSQLHOST,
+    port: process.env.DB_PORT || process.env.MYSQLPORT,
+    dialect: "mysql"
   },
   test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    username: process.env.DB_USER || process.env.MYSQLUSER,
+    password: process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+    database: process.env.DB_NAME || process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST || process.env.MYSQLHOST,
+    port: process.env.DB_PORT || process.env.MYSQLPORT,
+    dialect: "mysql"
   },
   production: {
-    use_env_variable: "mysql://root:PkupeZTNeNGCHURjtxeTRrNnQuIprlGk@interchange.proxy.rlwy.net:50038/railway",
+    use_env_variable: "DATABASE_URL",
     dialect: "mysql",
     dialectOptions: {
       ssl: {
